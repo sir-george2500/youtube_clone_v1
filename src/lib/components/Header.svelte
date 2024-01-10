f43ef40c83867885ec3781064b835edafd6e482f
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-10 11:29:45 +0000

    settle the struture of the searchBar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,17 @@
-<header>
-    <div>
+<script>
+	import SearchBar from "./SearchBar.svelte";
+
+</script>
+<header class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0">
+    <div class="flex gap-4 items-center">
+        <img src="/icons/bugerIcon.svg" alt="" class="p-3 rounded-full hover:bg-gray-200 cursor-pointer">
         <img src="/icons/ytLogo.svg" alt="">
     </div>
-    <div></div>
-    <div></div>
+    <SearchBar />
+    <div class="flex gap-3 items-center">
+        <img src="/icons/createIcon.svg" alt="" class="p-3 rounded-full hover:bg-gray-200 cursor-pointer">
+        <img src="/icons/notIcon.svg" alt="" class="p-3 rounded-full hover:bg-gray-200 cursor-pointer">
+        <img src="/avatar.jpg" alt="" class="bg-gray-200 h-8 w-8 rounded-full">
+
+    </div>
 </header>
