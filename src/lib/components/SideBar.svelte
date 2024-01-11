242f4b4e73c87ba038b85ab0f64a1764c2dcd3e5
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-11 22:29:13 +0000

    added the sidebar icons

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
-
+ const nav = [
+    {
+        icon:'homeIcon',
+        title:'home'
+    },
+    {
+        icon:'shortsIcon',
+        title:'shorts'
+    },
+    {
+        icon:'libraryIcon',
+        title:'library'
+    }
+ ]
 </script>
 
 <div class="bg-white py-2 flex flex-col px-1 fixed top-[60px]">
-    <a href="/" class="flex items-center justify-center flex-col gap-1 py-4">
-        <img src="icons/subIcon.svg" alt="">
-        <p>subcription</p>
+    {#each nav as item}
+    <a href="/" class="flex items-center justify-center flex-col gap-1 py-4 hover:bg-gray-100 rounded-lg">
+        <img src={`icons/${item.icon}.svg`} alt="">
+        <p>{item.title}</p>
     </a>
+    {/each}
 </div>
