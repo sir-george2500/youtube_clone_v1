8b1b5085074166ee60b53038c28fa462a66489ac
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-12 19:52:03 +0000

    started search page

---
+++ src/lib/components/ChannelCard.svelte
@@ -0,0 +1,5 @@
+<div class="flex gap-4 items-center">
+    <div class="max-w-[360px] w-full">
+        <img src="/avatar.jpg" alt="">
+    </div>
+</div>
---
+++ src/routes/result/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ChannelCard from "$lib/components/ChannelCard.svelte";
+
+</script>
+<div class="space-y-5">
+    <ChannelCard />
+    <ChannelCard />
+    <ChannelCard />    
+    <ChannelCard /> 
+    <ChannelCard /> 
+</div>
