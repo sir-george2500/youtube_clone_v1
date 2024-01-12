5c35e6ce9f7bd1c1840b7f660a23520a664a993c
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-12 19:14:01 +0000

    completed the initial setup for the home page

--- src/lib/components/VideoCard.svelte
+++ src/lib/components/VideoCard.svelte
@@ -3,19 +3,29 @@
 </script>
 
 
-<div>
-    <div></div>
-    <img src="/thumbnail.webp" alt="" class="sm:h-[208px] h-[250px] w-full object-cover rounded-2xl bg-gray-200">
-    <div>
-        <img src="/avatar.jpg" alt="" class="h-9 w-9 bg-gray-100 rounded-full">
-        <h3>
-            Svelt is not a hyped 
-        </h3>
-        <p>Clever Engineer</p>
-        <p>5.2m</p>
-        <p>
-            2 hrs ago
-        </p>
-
+<a href="/" class="relative">
+    <div class="relative">
+        <div class="absolute bg-black/80 text-white text-xs font-semibold rounded bottom-0 right-1 px-1">10:30</div>
+        <img src="/thumbnail.webp" alt="" class="sm:h-[208px] h-[250px] w-full object-cover rounded-2xl bg-gray-200">
     </div>
-</div>
+    <div class="flex gap-3 mt-3">
+        <img src="/avatar.jpg" alt="" class="h-9 w-9 bg-gray-100 rounded-full">
+        <div class="flex flex-col">
+            <h3 class="capitalize font-medium">
+                Svelt is not a hyped 
+            </h3>
+            <p class="text-gray-600 capitalize mt-1">
+                Clever Engineer
+            </p>
+            <div class="text-gray-600 text-sm flex items-center">
+                <span>
+                    5.2k views
+                </span>
+                <span class="h-1  w-1 rounded-full bg-gray-600 ml-2 mr-2"></span>
+                <span>
+                    2 hrs ago
+                </span>
+            </div>
+        </div>
+    </div>
+</a>
