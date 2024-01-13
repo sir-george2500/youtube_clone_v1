05044a8d8b5f811427329e29525d134927ea00f2
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-13 01:50:46 +0000

    completed the design for the SearchVideo

--- src/lib/components/SearchVideoCard.svelte
+++ src/lib/components/SearchVideoCard.svelte
@@ -1,3 +1,27 @@
-<div>
-    <img src="/thumbnail.webp" class="max-w-[360px] object-cover aspect-video rounded-2xl w-full items-center" alt="">
+<div class="flex gap-8 cursor-pointer  bg-white rounded-md ">
+    <img src="/thumbnail.webp" class="max-w-[360px] object-cover aspect-video rounded-2xl w-full items-center" alt=""/>
+    <div>
+        <h1 class="capitalize font-medium text-lg ">Svelt is not a hyped</h1>
+        <div class="flex items-center mb-2 text-sm">
+            <span class="text-gray-600 mb-1">
+                178K views
+            </span>
+            <span class="mx-2">•</span>
+            <span class="text-gray-600">
+                2 months ago
+            </span>
+        </div>
+        <div class="flex gap-2 items-center">
+            <img src="/avatar.jpg" alt="avatar" class="h-8 w-8">
+            <p class="text-xs">Clever Engineer</p>
+        </div>
+        <div >
+            <p class="text-xs text-gray-600 pt-4">The fastest way to learn Svelt is to code in it, keep working CR7 mentality</p>
+        </div>
+        <div class="flex item-center gap-2 mt-3">
+            <p class="rounded bg-gray-200 text-gray=800 text-xs font-semibold py-0.5 px-1">4K</p>
+            <p class="rounded bg-gray-200 text-gray=800 text-xs font-semibold py-0.5 px-1">4K</p>
+            <p class="rounded bg-gray-200 text-gray=800 text-xs font-semibold py-0.5 px-1">4K</p>
+        </div>
+    </div>
 </div>
