85a8b5a8b9dd1d1fadc61e2c4767563571a66805
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-17 06:02:28 +0000

    resolve the bug with home video type alias uncaught promise for avartar

--- src/routes/(groupLayout)/+page.svelte
+++ src/routes/(groupLayout)/+page.svelte
@@ -8,7 +8,7 @@
         try {
             const resolvedData = await data.contents;
             console.log("Resolved Data:", resolvedData);
-            content = resolvedData;
+            content = resolvedData || [];
         } catch (error) {
             console.error("Error fetching data:", error);
             content = [];
@@ -22,15 +22,15 @@
 
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-16">
     {#each content as { type, video }}
-        {#if type === 'video'}
+        {#if type === 'video' && video}
             <VideoCard
-                avatar={video.author.avatar?.[0].url}
-                publishedTimeText={video.publishedTimeText}
-                thumbnail={video.thumbnails[0].url}
-                title={video.title}
-                videoId={video.videoId}
-                views={video.stats.views}
-                channelName={video.author.title}
+                avatar={video.author?.avatar?.[0]?.url || ''}
+                publishedTimeText={video.publishedTimeText || ''}
+                thumbnail={video.thumbnails?.[0]?.url || ''}
+                title={video.title || ''}
+                videoId={video.videoId || ''}
+                views={video.stats?.views || 0}
+                channelName={video.author?.title || ''}
             />
         {/if}
     {/each}
